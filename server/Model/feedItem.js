ede221d0ec0c1700cfb69b55347cc3110615be49
GageFairbanks <108354958+GageFairbanks@users.noreply.github.com>
2022-07-15 22:07:58 -0400

    Update feedItem.js

--- server/Model/feedItem.js
+++ server/Model/feedItem.js
@@ -1,3 +1,4 @@
+// the main function that helps control the title, body, link, and image
 function feedItem (title, body, linkUrl, imageUrl) {
     this.title = title;
     this.body = body;
@@ -5,7 +6,7 @@ function feedItem (title, body, linkUrl, imageUrl) {
     this.imageUrl = imageUrl;
 }
 console.log('model running!');
-
+// returns the title the body the link and the image
 exports.createFeedItem = (title, body, linkUrl, imageUrl) => {
     return new feedItem(title, body, linkUrl, imageUrl);
 }
