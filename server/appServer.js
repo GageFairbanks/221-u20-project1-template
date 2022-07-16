cdbaa98d0d12d3a1e3907bd12002cc35db796e1f
GageFairbanks <108354958+GageFairbanks@users.noreply.github.com>
2022-07-15 22:02:28 -0400

    Update appServer.js

--- server/appServer.js
+++ server/appServer.js
@@ -1,8 +1,8 @@
 const express = require('express')
+const bodyPaser = require('body-parser');
 const app = express();
 app.use(express.static('client/public'));
-
-let feedController = require('./Controller/feedController');
+app.use(bodyParser.json({type: 'application/json'}));
 
 app.get('/', function(req, res) {
     res.sendFile('index.html', {root: './client/views'})
@@ -12,6 +12,30 @@ app.get('/feed', function(req, res) {
     res.sendFile('feed.html', {root: './client/views'})
 })
 
+//feed item
+let feedController = require('./Controller/feedController');
 
+app.route('/api/feedItem')
+    .get(feedController.getFeedItem)
+    .post(feedController.saveFeedItem);
+
+app.route('/api/feedItem/:feedItemId')
+    .get(feedController.getFeedItem)
+    .delete(feedController.deleteFeedItem)
+    .patch(feedController.updateFeedItem)
+    .put(feedController.updateFeedItem);
+
+//animal model
+let animalController = require('./Controller/animalController');
+
+app.route('/api/animalModel')
+    .get(animalController.getAnimalModel)
+    .post(animalController.saveAnimalModel);
+
+app.route('/api/animlModel/:animalModelId')
+    .get(animalController.getAnimalModel)
+    .delete(animalController.deleteAnimalModel)
+    .patch(animalController.updateAnimalModel)
+    .put(animalController.updateAnimalModel);
 
 app.listen(1337, () => console.log('Listening on port 1337.'))
