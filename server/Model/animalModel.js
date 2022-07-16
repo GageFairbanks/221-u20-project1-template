77082915fcc08fa362872c79246c3798bda829b5
GageFairbanks <108354958+GageFairbanks@users.noreply.github.com>
2022-07-15 22:06:56 -0400

    Update animalModel.js

--- server/Model/animalModel.js
+++ server/Model/animalModel.js
@@ -1,10 +1,11 @@
+//the main function that helps control the data
 function animalModel (title, body, size) {
     this.title = title;
     this.body = body;
     this.size = size;
 }
 console.log('model running!');
-
+// returns the title body and size
 exports.createAnimalModel = (title, body, size) => {
     return new animalModel(title, body, size);
 }
